fb34aeaf06750f01aab8b28465091e21dd0ade21
Ali <ali.bkp82@gmail.com>
2023-11-07 04:38:46 -0800

    add ez routing functoins

--- S06/S06.js
+++ S06/S06.js
@@ -4,10 +4,30 @@ server.listen(80);
 
 let headers = {"Content-Type" : "Text/Plain"};
 
+
+
 function requestHandler(request,response)
 {
+    let currentRoute = request.url.split('/');
+    let routes = 
+    {
+        sayHello : function ()
+        {
+            response.write("salam"+ " " + request.url + " "+  request.method + '\n');
+            response.end();
+        },
+        "saySomeThing":function()
+        {
+            response.write("<h1 style=' color:"+ currentRoute[3] +";'>"+ currentRoute[2]+ "</h1>" + '\n');
+            response.end();
+        },
+        "favicon.ico" : function()
+        {
+            response.write("fav" + '\n');
+            response.end();
+        }
+    }
 
-    response.writeHead(200,headers);
-    response.write("salam"+ " " + request.url + " "+  request.method);
-    response.end();
+    
+    routes[currentRoute[1]]();
 }

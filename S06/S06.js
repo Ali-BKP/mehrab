a1ca884fcc34a12bf487a0663c7a9e40444c2efa
Ali <ali.bkp82@gmail.com>
2023-11-07 04:52:13 -0800

    some optimize

--- S06/S06.js
+++ S06/S06.js
@@ -4,30 +4,31 @@ server.listen(80);
 
 let headers = {"Content-Type" : "Text/Plain"};
 
-
-
-function requestHandler(request,response)
-{
-    let currentRoute = request.url.split('/');
-    let routes = 
+let routes = 
     {
-        sayHello : function ()
+        sayHello : function (request,response,currentRoute)
         {
             response.write("salam"+ " " + request.url + " "+  request.method + '\n');
             response.end();
         },
-        "saySomeThing":function()
+        "saySomeThing":function(request,response,currentRoute)
         {
             response.write("<h1 style=' color:"+ currentRoute[3] +";'>"+ currentRoute[2]+ "</h1>" + '\n');
             response.end();
         },
-        "favicon.ico" : function()
+        "favicon.ico" : function(request,response,currentRoute)
         {
             response.write("fav" + '\n');
             response.end();
         }
     }
 
+function requestHandler(request,response)
+{
+    let currentRoute = request.url.split('/');
     
-    routes[currentRoute[1]]();
+    if(currentRoute[1] != "favicon.ico")
+    {
+        routes[currentRoute[1]](request,response,currentRoute);
+    }
 }
